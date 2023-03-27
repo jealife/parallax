a20800f5f3f345450adcb5c9284ed730e2633153
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-03-27 21:50:52 +0200

    Update scrolleffect.js

--- scrolleffect.js
+++ scrolleffect.js
@@ -3,10 +3,11 @@ let controller = new ScrollMagic.Controller();
 let timeline=new TimelineMax();
 
 timeline
-.to('.left',3 , {y:"200"})
+.to('.left',3 , {y:"20%", width:"90px", top:"1.2rem", position:"fixed"})
 .to('.main-tilte',3, {y:-400},"-=3")
 .to('.content',3.5, {backgroundColor:"#7501c8"})
-.fromTo('.content-images',{opacity:0}, {opacity:1, duration:5})
+.to('nav',1, {padding: "1rem 3rem",backgroundColor:"#0122c822",backdropFilter: "blur(50px)", position:"fixed"})
+// .fromTo('.content-images',{opacity:0}, {opacity:1,})
 
 let scene= new ScrollMagic.Scene({
     triggerElement: "section",
