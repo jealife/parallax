0eeb295ec48a624e6bf82774a7642b49ca7c3ff3
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-03-27 16:07:57 +0000

    Add files via upload

---
+++ scrolleffect.js
@@ -0,0 +1,17 @@
+// init controller
+let controller = new ScrollMagic.Controller();
+let timeline=new TimelineMax();
+
+timeline
+.to('.left',3 , {y:"20%"})
+.to('.main-tilte',3, {y:-400},"-=3")
+.to('.content',3.5, {backgroundColor:"#7501c8"})
+.fromTo('.content-images',{opacity:0}, {opacity:1, duration:5})
+
+let scene= new ScrollMagic.Scene({
+    triggerElement: "section",
+    duration:"100%",
+    triggerHook:0,
+})
+    .setTween(timeline)
+    .addTo(controller);
