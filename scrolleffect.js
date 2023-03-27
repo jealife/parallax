2d4df114157c98b10766a3c6bfadf27372ef1fdd
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-03-27 23:50:53 +0200

    Update scrolleffect.js

--- scrolleffect.js
+++ scrolleffect.js
@@ -3,10 +3,12 @@ let controller = new ScrollMagic.Controller();
 let timeline=new TimelineMax();
 
 timeline
-.to('.left',3 , {y:"23%"})
+.to('nav',1, {padding: "1rem 3rem",position:"fixed",backgroundColor:"#0122c822",backdropFilter: "blur(50px)"})
+.to('.left',3 , {y:"-200"})
 .to('.main-tilte',3, {y:-400},"-=3")
+.to('.bg',3, {y:50,width:"++"},"-=3")
 .to('.content',3.5, {backgroundColor:"#7501c8"})
-.to('nav',1, {padding: "1rem 3rem",backgroundColor:"#0122c822",backdropFilter: "blur(50px)", position:"fixed"})
+.to('nav',1, {position:"fixed",padding: "1rem 3rem",backgroundColor:"#0122c822",backdropFilter: "blur(50px)"})
 // .fromTo('.content-images',{opacity:0}, {opacity:1,})
 
 let scene= new ScrollMagic.Scene({
@@ -16,3 +18,5 @@ let scene= new ScrollMagic.Scene({
 })
     .setTween(timeline)
     .addTo(controller);
+
+////slidescrip
